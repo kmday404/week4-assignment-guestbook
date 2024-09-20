3306eb1ef31556b45be51856281c94f3e40a1837
Katherine Day <kmday125@gmail.com>
2024-09-20 09:11:31 +0100

    updates to server.js file

--- server/server.js
+++ server/server.js
@@ -9,7 +9,11 @@ dotenv.config();
 app.use(express.json());
 app.use(cors());
 
-//------
+const dbConnectionString = process.env.DATABASE_URL;
+const db = new pg.Pool({
+  connectionString: dbConnectionString,
+});
+
 app.listen(8080, () => {
   console.log(`Server running on PORT 8080`);
 });
@@ -17,3 +21,13 @@ app.listen(8080, () => {
 app.get("/", (req, res) => {
   res.json({ message: "This is the root route!" });
 });
+
+//I need an endpoint to READ the data in my pets table
+//We can use SQL to query our database and get the data
+//Our database is not hosted locally, so we don't know how long the data takes to travel
+// app.get("/pets", async (req, res) => {
+//   const query = await db.query(`SELECT * FROM pets`);
+//   //we can wrangle the query response to get the rows property only.
+//   res.json(query.rows);
+//   console.log(query);
+// });
